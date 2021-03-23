8ee08094d5c3832a8bed45cc470d667d41d7efcd
andrewshiva <andrewshiva886@gmail.com>
2021-03-23 19:35:10 +0530

    new

---
+++ script.js
@@ -0,0 +1,35 @@
+function test(a, b) {
+    //addiction
+    console.log(a + b);
+    //multiplication
+
+    console.log(a * b);
+    //division 
+
+    console.log(a / b);
+    //subtraction
+    console.log(a - b);
+    //Modulus
+    console.log(a % b);
+}
+test(70, 10);
+//third program
+var array = [10, 2, 3, 40, 5, 60],
+    s = 0,
+
+    i;
+for (i = 0; i < array.length; i += 1) {
+    s += array[i];
+
+}
+console.log('Sum : ' + s);
+//third program finished here 
+//second program working on it 
+
+var a = 39;
+var b = 5;
+
+function call() {
+    var num = ~~(a / b);
+   
+}
